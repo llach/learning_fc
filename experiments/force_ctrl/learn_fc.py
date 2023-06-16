0c16f4a0d32a9e1e8ea53c376f4482f8f9d67152
llach <lachluca@gmail.com>
2023-06-16 10:48:11 +0200

    experiment: obs_q_f_df

--- experiments/force_ctrl/learn_fc.py
+++ experiments/force_ctrl/learn_fc.py
@@ -1,7 +1,12 @@
 from learning_fc.training import train
 
-train(
-    env_name="gripper_tactile", 
-    model_name="ppo",
-    logdir="/tmp/tactile/",
-)
+name = "obs_q_f_df"
+plot_title = "OBS={q, force, deltaf}"
+
+for alg in ["ppo", "td3"]:
+    train(
+        env_name="gripper_tactile", 
+        model_name=alg,
+        name=name,
+        plot_title=plot_title,
+    )
