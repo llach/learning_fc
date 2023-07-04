cad7a31a7dc7b9e0d566c633e38107cdab2504ee
llach <lachluca@gmail.com>
2023-07-04 14:34:43 +0200

    more envs, longer training → better control policies

--- experiments/train_model.py
+++ experiments/train_model.py
@@ -9,8 +9,8 @@ if __name__ == "__main__":
     train(
         env_name="gripper_tactile", 
         model_name=ALG,
-        nenv=4,
-        train_kw=dict(timesteps=4e5),
+        nenv=6,
+        train_kw=dict(timesteps=1e6),
         env_kw=dict(
             control_mode=CTRL, 
             obs_config=OBS, 
