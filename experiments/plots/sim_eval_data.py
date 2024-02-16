11f497f2b21ecf3ab568fb59686572f51c31d448
llach <lachluca@gmail.com>
2024-02-16 13:57:04 +0100

    simulation experiments

--- experiments/plots/sim_eval_data.py
+++ experiments/plots/sim_eval_data.py
@@ -10,47 +10,48 @@ from learning_fc.training.evaluation import make_eval_env_model
 
 dist = 0.1
 width = 0.15
-ntrials = 100
+ntrials = 200
 kappas = np.arange(0,1.1,0.2)
 
 if __name__ == "__main__":
 
     trial = find_latest_model_in_path(model_path, filters=["ppo"])
-    env, model, _, _ = make_eval_env_model(f"{model_path}/2024-02-11_16-40-14__gripper_tactile__ppo__k-3__lr-0.0006", with_vis=0, checkpoint="best")
+    env, model, _, _ = make_eval_env_model(f"{model_path}/2024-02-11_16-40-14__gripper_tactile__ppo__k-3__lr-0.0006_M2_inbnew", with_vis=0, checkpoint="best")
 
     # use force reward only TODO should we also use the action penalty?
     env.set_attr("rp_scale", 0.0)
     env.set_attr("ro_scale", 0.0)
     env.set_attr("ra_scale", 0.0)
     env.set_attr("ah_scale", 0.0)
+    env.set_attr("with_bias", False)
 
     """ Force Controller (Baseline)
     """
 
-    # fc = ForcePI(env)
+    fc = ForcePI(env)
 
-    # start = time.time()
-    # fc_res = []
-    # fc_objd = []
-    # for kappa in kappas:
-    #     crs = []
-    #     objds = []
-    #     for _ in range(ntrials):
-    #         fc.reset()
-    #         cr, objd = get_cumr_obj_delta(env, fc)
-    #         crs.append(cr)
-    #         objds.append(objd)
-    #     fc_objd.append(objds)
-    #     fc_res.append(crs)
-    # print(f"fc took {time.time()-start}")
+    start = time.time()
+    fc_res = []
+    fc_objd = []
+    for kappa in kappas:
+        crs = []
+        objds = []
+        for _ in range(ntrials):
+            fc.reset()
+            cr, objd = get_cumr_obj_delta(env, fc)
+            crs.append(cr)
+            objds.append(objd)
+        fc_objd.append(objds)
+        fc_res.append(crs)
+    print(f"fc took {time.time()-start}")
 
-    # print(np.mean(fc_res), np.std(fc_res))
+    print(np.mean(fc_res), np.std(fc_res))
 
-    # with open(f"{model_path}/fc_eval.pkl", "wb") as f:
-    #     pickle.dump({
-    #         "res": fc_res,
-    #         "objd": fc_objd,
-        # }, f)
+    with open(f"{model_path}/fc_eval.pkl", "wb") as f:
+        pickle.dump({
+            "res": fc_res,
+            "objd": fc_objd,
+        }, f)
    
     """ Policy
     """
@@ -77,60 +78,82 @@ if __name__ == "__main__":
 
     """ Policy - without inductive bias
     """
-    # start = time.time()
-    # noib_res = []
-    # model.set_parameters(f"{model_path}/2023-09-14_11-24-22__gripper_tactile__ppo__k-3__lr-0.0006_M2_noinb/weights/_best_model.zip")
-    # for kappa in kappas:
-    #     noib_res.append([
-    #         get_cumr_obj_delta(env, model) for _ in range(ntrials)
-    #     ])
-    # noib = np.array(noib_res)
-    # noib_res = noib[:,:,0]
-    # noib_objd = noib[:,:,1]
-    # print(f"noib took {time.time()-start}")
+    start = time.time()
+    noib_res = []
+    model.set_parameters(f"{model_path}/2023-09-14_11-24-22__gripper_tactile__ppo__k-3__lr-0.0006_M2_noinb/weights/_best_model.zip")
+    for kappa in kappas:
+        noib_res.append([
+            get_cumr_obj_delta(env, model) for _ in range(ntrials)
+        ])
+    noib = np.array(noib_res)
+    noib_res = noib[:,:,0]
+    noib_objd = noib[:,:,1]
+    print(f"noib took {time.time()-start}")
 
-    # with open(f"{model_path}/noib_eval.pkl", "wb") as f:
-    #     pickle.dump({
-    #         "res": noib_res,
-    #         "objd": noib_objd,
-    #     }, f)
+    with open(f"{model_path}/noib_eval.pkl", "wb") as f:
+        pickle.dump({
+            "res": noib_res,
+            "objd": noib_objd,
+        }, f)
 
     """ Policy - without domain randomization
     """
-    # start = time.time()
-    # nodr_res = []
-    # model.set_parameters(f"{model_path}/2023-09-15_08-22-36__gripper_tactile__ppo__k-3__lr-0.0006_M2_nor/weights/_best_model.zip")
-    # for kappa in kappas:
-    #     nodr_res.append([
-    #         get_cumr_obj_delta(env, model) for _ in range(ntrials)
-    #     ])
-    # nodr = np.array(nodr_res)
-    # nodr_res = nodr[:,:,0]
-    # nodr_objd = nodr[:,:,1]
-    # print(f"nodr took {time.time()-start}")
+    start = time.time()
+    nodr_res = []
+    model.set_parameters(f"{model_path}/2023-09-15_08-22-36__gripper_tactile__ppo__k-3__lr-0.0006_M2_nor/weights/_best_model.zip")
+    for kappa in kappas:
+        nodr_res.append([
+            get_cumr_obj_delta(env, model) for _ in range(ntrials)
+        ])
+    nodr = np.array(nodr_res)
+    nodr_res = nodr[:,:,0]
+    nodr_objd = nodr[:,:,1]
+    print(f"nodr took {time.time()-start}")
 
-    # with open(f"{model_path}/nodr_eval.pkl", "wb") as f:
-    #     pickle.dump({
-    #         "res": nodr_res,
-    #         "objd": nodr_objd,
-    #     }, f)
+    with open(f"{model_path}/nodr_eval.pkl", "wb") as f:
+        pickle.dump({
+            "res": nodr_res,
+            "objd": nodr_objd,
+        }, f)
         
     """ Policy - without learning curriculum
     """
-    # start = time.time()
-    # nocurr_res = []
-    # model.set_parameters(f"{model_path}/2024-02-05_13-51-07__gripper_tactile__ppo__k-3__lr-0.0006_M2_nocurr/weights/_best_model.zip")
-    # for kappa in kappas:
-    #     nocurr_res.append([
-    #         get_cumr_obj_delta(env, model) for _ in range(ntrials)
-    #     ])
-    # nocurr = np.array(nocurr_res)
-    # nocurr_res = nocurr[:,:,0]
-    # nocurr_objd = nocurr[:,:,1]
-    # print(f"nocurr took {time.time()-start}")
+    start = time.time()
+    nocurr_res = []
+    model.set_parameters(f"{model_path}/2024-02-05_13-51-07__gripper_tactile__ppo__k-3__lr-0.0006_M2_nocurr/weights/_best_model.zip")
+    for kappa in kappas:
+        nocurr_res.append([
+            get_cumr_obj_delta(env, model) for _ in range(ntrials)
+        ])
+    nocurr = np.array(nocurr_res)
+    nocurr_res = nocurr[:,:,0]
+    nocurr_objd = nocurr[:,:,1]
+    print(f"nocurr took {time.time()-start}")
 
-    # with open(f"{model_path}/fc_models/nocurr_eval.pkl", "wb") as f:
-    #     pickle.dump({
-    #         "res": nocurr_res,
-    #         "objd": nocurr_objd,
-    #     }, f)
+    with open(f"{model_path}/nocurr_eval.pkl", "wb") as f:
+        pickle.dump({
+            "res": nocurr_res,
+            "objd": nocurr_objd,
+        }, f)
+
+    """ Policy - without object movement penalty
+    """
+    start = time.time()
+    nopen_res = []
+    model.set_parameters(f"{model_path}/2024-02-11_21-26-18__gripper_tactile__ppo__k-3__lr-0.0006_M2_nopen/weights/_best_model.zip")
+    for kappa in kappas:
+        nopen_res.append([
+            get_cumr_obj_delta(env, model) for _ in range(ntrials)
+        ])
+    nopen = np.array(nopen_res)
+    nopen_res = nopen[:,:,0]
+    nopen_objd = nopen[:,:,1]
+    print(f"nopen took {time.time()-start}")
+    print(f"rew {np.mean(nopen_res)}±{np.std(nopen_res)}")
+    print(f"obj {np.mean(nopen_objd)}±{np.std(nopen_objd)}")
+
+    with open(f"{model_path}/nopen_eval.pkl", "wb") as f:
+        pickle.dump({
+            "res": nopen_res,
+            "objd": nopen_objd,
+        }, f)
--- experiments/plots/sim_eval_plot.py
+++ experiments/plots/sim_eval_plot.py
@@ -6,35 +6,36 @@ import matplotlib.pyplot as plt
 from learning_fc import model_path
 from sim_eval_data import dist, width, kappas
 
- # legend 
-plt.rcParams['legend.fancybox']  = False
-plt.rcParams['legend.edgecolor'] = "#6C6C6D"
+# legend 
+plt.rcParams['legend.fancybox']  = True
+plt.rcParams['legend.edgecolor'] = "#656565"
 
-# # axes
+# axes
+plt.rcParams['axes.spines.top'] = False
+plt.rcParams['axes.spines.right'] = False
 plt.rcParams['xtick.direction'] = "in"
 plt.rcParams['ytick.direction'] = "in"
-# plt.rcParams['lines.linewidth'] = 1.5
+plt.rcParams['lines.linewidth'] = 1.2
 
-plt.rcParams['font.size'] = 23
-plt.rcParams['legend.fontsize'] = 16
-plt.rcParams['xtick.labelsize'] = 17
-plt.rcParams['ytick.labelsize'] = 17
+plt.rcParams['font.family'] = 'serif'
+plt.rcParams['font.size'] = 15
+plt.rcParams['font.weight'] = "medium"
+plt.rcParams['legend.fontsize'] = 12
+plt.rcParams['xtick.labelsize'] = 12
+plt.rcParams['ytick.labelsize'] = 12
 
-# plt.rcParams['font.family'] = 'serif'
+plt.rcParams["figure.dpi"] = 200
+plt.rcParams["savefig.format"] = "pdf"
+plt.rcParams['figure.constrained_layout.use'] = True
 
-# high quality figure
-plt.rcParams["figure.dpi"] = 100
-# plt.rcParams["savefig.format"] = "pdf"
-# plt.rcParams['figure.constrained_layout.use'] = True
 
-# use LaTeX
-# plt.rcParams['text.usetex'] = True
-# plt.rcParams['text.latex.preamble'] = r'\usepackage{amsmath}\usepackage[T1]{fontenc}'
+plt.rcParams['text.usetex'] = True
+plt.rcParams['text.latex.preamble'] = r'\usepackage{amsmath}\usepackage[T1]{fontenc}'
 
 def configure_boxes(box, ax):
 
     # Customizing box colors
-    colors = ['#4CAF50', '#2196F3', '#FFC107', '#FF5722', '#9C27B0']
+    colors = ['#4CAF50', '#2196F3', '#FFC107', '#FF5722', '#9C27B0', '#607D8B']
     for patch, color in zip(box['boxes'], colors):
         patch.set_facecolor(color)
 
@@ -57,10 +58,11 @@ def configure_boxes(box, ax):
     # Customizing tick labels
     ax.set_xticklabels([
         f'Baseline', 
-        r"$\pi^{IB}$",
-        r"$\pi^{NO-IB}$",
-        r"$\pi^{NO-RAND}$",
-        r"$\pi^{NO-CURR}$",
+        r"$\pi^\text{\small IB}$",
+        r"$\pi^\text{\small NO-IB}$",
+        r"$\pi^\text{\small NO-PEN}$",
+        r"$\pi^\text{\small NO-RAND}$",
+        r"$\pi^\text{\small NO-CURR}$",
     ], rotation=45, fontsize=14)
 
     # Remove top and right spines
@@ -71,47 +73,25 @@ def configure_boxes(box, ax):
 force_rewards = []
 objd = []
 
-for mname in "fc,pol,noib,nodr,nocurr".split(","):
+for mname in "fc,pol,noib,nopen,nodr,nocurr".split(","):
     with open(f"{os.environ['HOME']}/fc_models/{mname}_eval.pkl", "rb") as f:
         data = pickle.load(f)
         force_rewards.append(np.array(data["res"]).reshape((-1)))
-        objd.append(np.array(data["objd"]).reshape((-1))*100)
+        objd.append(np.array(data["objd"]).reshape((-1))*1000)
 
-fig, (ax1, ax2) = plt.subplots(ncols=2, figsize=(7.8, 5.5))
+fig, (ax1, ax2) = plt.subplots(ncols=2)
 
 box  = ax1.boxplot(force_rewards, showfliers=False, widths=0.25, patch_artist=True)
 configure_boxes(box, ax1)
 
-# for patch, color in zip(rew_bpl["boxes"], ("pink", "lightblue", "#05B24A", "#c77dff")):
-#     patch.set_facecolor(color)
-
-# ax1.legend(
-#     [
-#         fc_bpl["boxes"][0], 
-#         pol_bpl["boxes"][0],
-#         noib_bpl["boxes"][0],
-#         nodr_bpl["boxes"][0], 
-#     ], 
-#     [
-#         f'Baseline', 
-#         r"$\pi^{IB}$",
-#         r"$\pi^{NO-IB}$",
-#         r"$\pi^{NO-RAND}$",
-#     ],
-#     loc='upper center', 
-#     bbox_to_anchor=(0.5, 1.13),
-#     ncol=4,
-# )
-
 ax1.set_title("Force Reward", fontsize=20)
 ax2.set_title("Object Displacement", fontsize=20)
 
-ax1.set_ylabel(r"$r$", fontsize=16)
-ax2.set_ylabel(r"$\Delta o_y$ [cm]", fontsize=16)
+ax1.set_ylabel(r"$r^\text{force}$", fontsize=16)
+ax2.set_ylabel(r"$\Delta o_y$ [mm]", fontsize=16)
 
 objd_bpl  = ax2.boxplot(objd, widths=0.25, showfliers=False, patch_artist=True)
 configure_boxes(objd_bpl, ax2)
 
-fig.tight_layout()
 plt.savefig(f"{model_path}/box.pdf")
 plt.show()
