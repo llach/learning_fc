92118de5aeb4b08671422b71d564a76ca44b9a3a
llach <lachluca@gmail.com>
2024-04-11 11:49:20 +0200

    new grasp plot

--- experiments/plots/grasp_plot.py
+++ experiments/plots/grasp_plot.py
@@ -1,11 +1,17 @@
+import os
 import pickle
-import matplotlib.pyplot as plt
-from matplotlib.offsetbox import AnchoredOffsetbox, TextArea, VPacker
-import matplotlib as mpl
-
+import pathlib
 import numpy as np
+import matplotlib as mpl
+from matplotlib import colormaps as cm
+import matplotlib.pyplot as plt
 
-pklpath = "data/external_forces_FC_28.02.2022_12:09:45.pkl"
+from learning_fc import cm_uni, cm_itten
+import plot_config
+cmap = cm["tab10"]
+
+
+pklpath = f"{pathlib.Path(__file__).parent.absolute()}/data/external_forces_FC_28.02.2022_12:09:45.pkl"
 with open(pklpath, "rb") as f:
     try:
         data = pickle.load(f, encoding="latin1") # Python3
@@ -30,51 +36,49 @@ targetF = data['targetF']
 
 deltaT -= deltaT[0]
 
-mpl.rcParams['text.latex.preamble'] = r'\usepackage{amsmath}'
-mpl.rcParams['mathtext.fontset'] = 'cm'
-
-fig, axes = plt.subplots(nrows=3, ncols=1, figsize=(10, 9))
+fig, axes = plt.subplots(nrows=3, ncols=1, figsize=(8,7.5))
 
 # f1, f2
 # multi-color y-axis label
 c1 = "#FF6600"
 c2 = "#004E78"
 
-label_fs = 17
-legend_fs = 16
 noise_t_color = color='#202020'
 
-axes[0].plot(deltaT, f[:,0], label='$f_1$', color=c1)
-axes[0].plot(deltaT, f[:,1],  label='$f_2$', color=c2)
+axes[0].plot(deltaT, f[:,0], label=r'$f_1$', color=cm_itten["orange"]) 
+axes[0].plot(deltaT, f[:,1],  label=r'$f_2$', color=cm_uni["blue"])
 
-axes[0].text(0, noiseT+0.3, "$f_{\\theta}}$", fontsize="x-large")
+axes[0].text(0.2, noiseT+0.3, r"$f_\theta$", fontsize=12)
 
-axes[0].axhline(y=noiseT, color=noise_t_color, linestyle='--', linewidth=1.0)
-axes[0].axhline(y=-noiseT, color=noise_t_color, linestyle='--', linewidth=1.0)
+axes[0].axhline(y=noiseT, color=noise_t_color, linestyle=(0, (5, 5)), linewidth=0.5)
+axes[0].axhline(y=-noiseT, color=noise_t_color, linestyle=(0, (5, 5)), linewidth=0.5)
 
-axes[0].set_ylabel("Force measurements [$N$]", fontsize=label_fs-2.3)
-axes[0].legend(prop={'size': legend_fs})
+axes[0].set_ylabel(r"Forces $f_i$ \, [$N$]")
+leg = axes[0].legend()
+leg.get_frame().set_linewidth(0.3)
+
 
 # f1 + f2
-axes[1].set_ylabel("Internal object force [$N$]", fontsize=label_fs)
-axes[1].text(-0.05, targetF+0.15, "$f^{\\,\\mathrm{goal}}$", fontsize="x-large")
+axes[1].set_ylabel(r"Internal object force $f^\text{int}$ \, [$N$]")
+# axes[1].text(0.88, targetF+0.15, r"$f^\text{goal}$" , fontsize=12)
 
-axes[1].plot(deltaT, fAdd, c="#FFB14E", label="$f_1 + f_2$")
-axes[1].axhline(y=targetF, color='#585858', linestyle='-', linewidth=1.0)
-axes[1].legend(prop={'size': legend_fs})
+axes[1].plot(deltaT, fAdd, c=cm_uni["darkgreen"], label=r"$f_1 + f_2$")
+axes[1].axhline(y=targetF, color=cmap(7), linestyle='-', linewidth=1.0, label=r"$f^\text{goal}$")
+leg = axes[1].legend()
+leg.get_frame().set_linewidth(0.3)
 
 axes[1].set_ylim(-0.2, 4.5)
 
 # f1 - f2 + mgn
-axes[2].set_ylabel("External force [$N$]", fontsize=label_fs)
-fdiffplot, = axes[2].plot(deltaT, fDiff, c="#EA5F94", label="$f_1 - f_2 + m\\,\\mathbf{g}\\cdot\\mathbf{n}$")
+axes[2].set_ylabel(r"External force $f^\text{ext}$ \, [$N$]")
+fdiffplot, = axes[2].plot(deltaT, fDiff, c=cm_itten["pink"], label=r"$f_1 - f_2 + m\,\mathbf{g}\cdot\mathbf{n}$")
 
 frac=2
-axes[2].text(0, (frac*driftT)+0.8, "$f_{\\phi}}$", fontsize="x-large")
+axes[2].text(0.2, (frac*driftT)+0.8, r"$f_\phi$", fontsize=12)
 axes[2].set_ylim(-3.7, 3.7)
 
-axes[2].axhline(y=frac*driftT, color=noise_t_color, linestyle='--', linewidth=1.0)
-axes[2].axhline(y=-frac*driftT, color=noise_t_color, linestyle='--', linewidth=1.0)
+axes[2].axhline(y=frac*driftT, color=noise_t_color, linestyle='--', linewidth=0.5)
+axes[2].axhline(y=-frac*driftT, color=noise_t_color, linestyle='--', linewidth=0.5)
 
 ax2t = axes[2].twinx()
 color_ot = "#EE0000"
@@ -82,49 +86,43 @@ color_ot = "#EE0000"
 # ot can only be calculated after both fingers touch -> correct for that offset (before force closure, the following calculation is biased by the non-touching finger's trajectory)
 ot = (((qDes[:, 0]-qDes[:, 1])/2)*1000) - 8.2
 ot[0:245] = ot[240:485]
-otplot, = ax2t.plot(deltaT, ot, c="blue", label="$O(t)$")
-ax2t.set_ylabel("Object position $O(t)$", fontsize=label_fs)
-# ax2t.text(-0.1, 0.002, "$O(t = \\mathrm{III})$", fontsize="x-large")
-# ax2t.tick_params(axis="y",direction="in", pad=-25)
+otplot, = ax2t.plot(deltaT, ot, c=cm_uni["blue"], label=r"$O(t)$")
+ax2t.set_ylabel(r"Object position $O(t)$")
 
-axes[2].legend([fdiffplot, otplot], ["$f_1 - f_2 + m\\,\\mathbf{g}\\cdot\\mathbf{n}$", "$O(t)$"], loc=3, prop={'size': legend_fs})
+leg = axes[2].legend([fdiffplot, otplot], [r"$f_1 - f_2 + m\,\mathbf{g}\cdot\mathbf{n}$", r"$O(t)$"], loc=3)
+leg.get_frame().set_linewidth(0.3)
 
 for ax in axes[:-1].flatten():
-    ax.get_xaxis().set_visible(False) # hide xlabels for all but last rows
-    
+    ax.get_xaxis().set_ticklabels([]) # hide x axis tick labels for all but last rows
+axes[-1].spines[['right']].set_visible(True)
+
 for ax in axes:
-    ax.tick_params(axis='both', which='both', labelsize=13)
-ax2t.tick_params(axis='both', which='both', labelsize=13)
+    ax.spines[['right']].set_visible(True)
+    ax.set_xlim(0,13.5)
 
-phases = [(0, 1.82, "Closing"),
-          (1.82, 5.43, "Holding"),
-          (5.43, 6.94, "Right"),
-          (6.94, 9.42, "Relaxing"),
-          (9.42, 10.72, "Left"),
-          (10.72, deltaT[-1], "Relaxing")]
-
-rhe = 1.05
-pad=0.01
-for start, end, text in phases:
-    a = axes[0]
-    a.add_patch(mpl.patches.FancyBboxPatch(
-        (start+pad, rhe), width=end-start-2*pad, height=0.13, color="silver", transform=a.get_xaxis_transform(),
-        clip_on=False, boxstyle="round,pad=0,rounding_size=0.04", mutation_aspect=1))
-    a.text((start+end)/2, rhe+0.13/2, text, transform=a.get_xaxis_transform(), ha='center', va='center', fontsize=13)
-
-events = [(0.69, "1st touch"), (1.82, "2nd touch"), (5.43, ""), (6.94, ""), (9.42, ""), (10.72, "")]
+events = [(0.69, r"1$^\text{st}$ touch"), (1.82, r"2$^\text{nd}$ touch"), (5.43, ""), (6.94, ""), (9.42, ""), (10.72, "")]
 for i, a in enumerate(axes):
     for j, (x, text) in enumerate(events):
         if i == 0:
-            offset = 0.17 if j == 0 else 0.13
+            offset = 0.22 if j == 0 else 0.18
             yoff = 0 if j == 0 else 0.1
-            a.text(x+offset, 0.9-yoff, text, transform=a.get_xaxis_transform(), ha='center', va='center', fontsize="11")
-        a.axvline(x=x, linestyle='--', color='gray', linewidth=1.)
+            a.text(x+offset, 0.9-yoff, text, transform=a.get_xaxis_transform(), ha='center', va='center')
+        a.axvline(x=x, linestyle=(0,(5,5)), color='gray', linewidth=0.8)
+
+axes[-1].set_xlabel(r"Time [$s$]")
 
 fig.align_ylabels()
-fig.text(0.5, 0.007, 'time [$s$]', fontsize=18, ha='center')
-fig.tight_layout(rect=[0, 0.01, 1, 1])
-fig.subplots_adjust(hspace=0.1)
-fig.subplots_adjust(wspace=0.005)
-plt.savefig("grasp_new.pdf", dpi=300)
+# fig.text(0.5, 0.007, 'time [$s$]', fontsize=18, ha='center')
+# fig.tight_layout(rect=[0, 0.01, 1, 1])
+# fig.subplots_adjust(hspace=0.1)
+# fig.subplots_adjust(wspace=0.005)
+plt.savefig(f"{os.environ['HOME']}/repos/diss/images/ctrl/grasp_new.pdf")
 plt.show()
+
+
+# phases = [(0, 1.82, "Closing"),
+#           (1.82, 5.43, "Holding"),
+#           (5.43, 6.94, "Right"),
+#           (6.94, 9.42, "Relaxing"),
+#           (9.42, 10.72, "Left"),
+#           (10.72, deltaT[-1], "Relaxing")]
